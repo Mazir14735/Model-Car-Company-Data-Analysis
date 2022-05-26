706bf24a8f785de6d5893bc7d01d099032469520
mazir14735 <amazigh.selhi@gmail.com>
2022-05-26 19:32:30 +0200

    requete dette faite

---
+++ Clients_with_debt.sql
@@ -0,0 +1,10 @@
+SELECT ordered, a.customerNumber, payed, (ordered-payed) as difference
+from
+(Select SUM(OD.priceEach * OD.quantityOrdered)as ordered, O.customerNumber
+FROM orderdetails OD
+JOIN orders O
+ON OD.orderNumber = O.orderNumber
+group by O.customerNumber)a
+join (select sum(amount) as payed, customerNumber from payments
+group by customerNumber)b
+on a.customerNumber = b.customerNumber
